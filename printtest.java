859ac04e85132e76c2d7de901db57f6019c0ea0b
AnithaPM <47944983+AnithaPM@users.noreply.github.com>
2019-02-26 17:53:56 +0530

    Create printtest.java
    new file to add numbers

---
+++ printtest.java
@@ -0,0 +1,10 @@
+public class printtest {
+   
+    public static void main(String[] args) {
+        int i=4,j=5,k;
+        k=i+j;
+        System.out.println("The addition of "+i+" and "+j+" is "+k);
+        System.out.printf("The addition of %d and %d is %d",i,j,k);
+    }
+}
+
