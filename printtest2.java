79ec8d53645bbad53b829d2ab25bfbbe7dd71f0a
unknown <anithadxb2000@gmail.com>
2019-02-26 18:20:14 +0530

    changed the numbers

--- printtest2.java
+++ printtest2.java
@@ -1,7 +1,7 @@
 public class printtest2 {
    
     public static void main(String[] args) {
-        int i=4,j=5,k;
+        int i=6,j=7,k;
         k=i+j;
         System.out.println("The addition of "+i+" and "+j+" is "+k);
         System.out.printf("The addition of %d and %d is %d",i,j,k);
